9148861b3e9369cde0662f1ccf290afd04bd666a
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-16 23:20:31 +0200

    initialize this as Spring Boot project by (starting from the parent directory and issuing the following commands): ```shell $ curl https://start.spring.io/starter.zip \     -d type=maven-project \     -d language=java \     -d bootVersion=4.1.0 \     -d groupId=com.github \     -d artifactId=tutorial-about-java-20 \     -d packageName=com.github.tutorial-about-java-20 \     -d packaging=jar \     -d javaVersion=25 \     -d dependencies=web,thymeleaf,devtools \     -o tutorial-about-java-20.zip
    $ unzip tutorial-about-java-20.zip \
        -d tutorial-about-java-20
    
    $ rm tutorial-about-java-20.zip
    ```

---
+++ src/main/java/com/github/tutorial_about_java_20/TutorialAboutJava20Application.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAboutJava20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAboutJava20Application.class, args);
+	}
+
+}
