c6734a3c2a30640e1a215dc0bc36d346a46d51af
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-17 00:09:31 +0200

    create a `GreetingController` that uses Thymeleaf to parse the returned HTML template, "fill it in", and send the "filled-in" HTML directly back to the caller

---
+++ README.md
@@ -0,0 +1,50 @@
+# run the application from the command line with ... Maven
+
+```shell
+$ ./mvnw spring-boot:run
+```
+
+(Either use a web browser or)
+Use another terminal window to ssue these HTTP request:
+```shell
+$ curl localhost:8080/greeting
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Getting Started: Serving Web Content</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+    <p >Hello, World!</p>
+</body>
+</html>
+
+
+
+$ curl 'localhost:8080/greeting?name=Mary'
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Getting Started: Serving Web Content</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+    <p >Hello, Mary!</p>
+</body>
+</html>
+```
+
+# build a single executable JAR file that contains all the necessary dependencies, classes, and resources and run that
+
+build the JAR file with
+```shell
+./mvnw clean package
+```
+
+run the JAR file, as follows:
+```
+java \
+    -jar target/tutorial-about-java-20-0.0.1-SNAPSHOT.jar
+```
+
+Issue the above-mentioned HTTP request (either via a web browser or via `curl`).
---
+++ src/main/java/com/github/tutorial_about_java_20/GreetingController.java
@@ -0,0 +1,41 @@
+package com.github.tutorial_about_java_20;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+[This class] handles GET requests for `/greeting` by returning the name of a `View`
+[- look for the HTML file with whose name matches the return value.]
+*/
+@Controller
+public class GreetingController {
+
+    /*
+    [This] annotation ensures that
+    HTTP GET requests to `/greeting` are mapped to the `greeting()` method.
+
+    The implementation of the method body relies on a view technology (in this case, Thymeleaf)
+    to perform server-side rendering of the HTML.
+    */
+    @GetMapping("/greeting")
+    public String greeting(
+        /*
+        [This annotation] binds the value of the query string parameter `name`
+        into the `name` parameter ofthe ... method.
+        */
+        @RequestParam(name="name", required=false, defaultValue="World")
+        String name,
+        Model model
+    ) {
+        // [The next statement is respnsible for]
+        // ultimately making [the value of the `name` parameter] accessible to the view template.
+        model.addAttribute("name", name);
+        // Thymeleaf parses the [HTML] template (whose name matches the return value)
+        // and
+        // evaluates the `th:text` expression to render the value of the `${name}` parameter that was set in the preceding statement.
+        return "greeting";
+    }
+    
+}
